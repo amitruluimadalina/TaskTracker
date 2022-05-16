7a98d1a1ef406ad53fb4453c343dbe92741818c6
amitruluimadalina <104475104+amitruluimadalina@users.noreply.github.com>
2022-05-16 04:35:16 +0300

    Add files via upload

---
+++ TaskTrackerVue-master/src/components/Task.vue
@@ -0,0 +1,35 @@
+<template>
+<div @dblclick="$emit('toggle-task', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>{{task.text}}
+        <i @click="$emit('delete-task', task.id)" class="fas fa-trash"></i>
+    </h3>
+    <p>{{task.day}}</p>
+</div>
+    
+</template>
+
+<script>
+    export default {
+        name:'Task',
+        props:{
+            task: Object,
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+    .task{
+        background-color: rgb(226, 226, 226);
+        margin: 0.7rem;
+        padding: 0.05rem 0.5rem;
+        cursor:pointer;
+    }
+    .task.reminder{
+        border-left: 0.4rem solid green;
+    }
+    .task h3{
+        display:flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
---
+++ TaskTrackerVue-master/src/components/Tasks.vue
@@ -0,0 +1,23 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <Task @toggle-task="$emit('toggle-task',task.id)" @delete-task="$emit('delete-task', task.id)" :task="task" />
+    </div>
+</template>
+
+<script>
+import Task from './Task'
+    export default {
+        name:'Tasks',
+        props:{
+            tasks: Array,
+        },
+        components: {
+            Task
+        },
+        emits:['delete-task', 'toggle-task'] 
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
